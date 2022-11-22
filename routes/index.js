71c3ca0bea5e3f8f23ccdb73a1f516d57795dc53
connoraleks <connoraleks@gmail.com>
2022-11-22 12:11:01 -0500

    launching AWS instance

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  console.log('index.js');
+  // send the index.html file from the apps/build-jaydnserranofrontend folder
+  res.sendFile('index.html', { root: 'apps/build-connoraleksfrontend' });
 });
 
 module.exports = router;
